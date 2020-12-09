d677e41de5f43e0cffdfcb1887f0a2511b63cd48
Monik09 <monikinderjitsingh5@gmail.com>
2020-12-10 01:08:02 +0530

    Added Social Media Icons

--- lib/screens/AboutScreen.dart
+++ lib/screens/AboutScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../widgets/appBarWidget.dart';
@@ -10,17 +11,18 @@ class AboutScreen extends StatelessWidget {
       backgroundColor: Color.fromRGBO(189, 224, 255, 1),
       appBar: appBarWidget(context),
       body: SingleChildScrollView(
-              child: Container(
+        child: Container(
           padding: EdgeInsets.symmetric(
               vertical: MediaQuery.of(context).size.height * 0.08),
           // color: Colors.white,
-          height: MediaQuery.of(context).size.height * 0.9,
+          height: MediaQuery.of(context).size.height * 0.8,
+          width: MediaQuery.of(context).size.width,
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                width: MediaQuery.of(context).size.width/2,
+                width: MediaQuery.of(context).size.width * 0.5,
                 child: Padding(
                   padding: const EdgeInsets.only(left: 70.0),
                   child: Column(
@@ -28,11 +30,17 @@ class AboutScreen extends StatelessWidget {
                     children: [
                       Text(
                         "Monikinderjit Singh",
-                      //   style: GoogleFonts.lora(
-                      //       color: Color(0xff173266), fontSize: 60,fontWeight: FontWeight.w800),
-                      // ),
-                      style:TextStyle(fontFamily: 'TheRichJuliet',fontSize: 60)),
-                      SizedBox(height: MediaQuery.of(context).size.height*0.1,),
+                        //   style: GoogleFonts.lora(
+                        //       color: Color(0xff173266), fontSize: 60,fontWeight: FontWeight.w800),
+                        // ),
+                        style: TextStyle(
+                          fontFamily: 'TheRichJuliet',
+                          fontSize: 60,
+                        ),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                      ),
                       Text(
                         "I love exploring new things and giving my best to make use of it",
                         softWrap: true,
@@ -43,16 +51,62 @@ class AboutScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(right:50.0),
-                child: Container(
-                  alignment: Alignment.center,
-                  child: CircleAvatar(
-                    radius: MediaQuery.of(context).size.width * 0.12,
-                    backgroundColor: Colors.black,
-                  ),
+              Container(
+                margin: EdgeInsets.only(
+                    right: MediaQuery.of(context).size.width * 0.12),
+                alignment: Alignment.center,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: MediaQuery.of(context).size.width * 0.12,
+                      backgroundColor: Colors.black,
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.01,
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Image.asset(
+                            'assets/images/linkedin.png',
+                          ),
+                          onPressed: () {},
+                          iconSize: 39,
+                        ),
+                        IconButton(
+                          icon: Image.asset(
+                            'assets/images/twitter.png',
+                          ),
+                          onPressed: () {},
+                          iconSize: 42,
+                        ),
+                        IconButton(
+                          icon: Image.asset(
+                            'assets/images/github.png',
+                          ),
+                          onPressed: () {},
+                          iconSize: 42,
+                        ),
+                        IconButton(
+                          icon: Image.asset(
+                            'assets/images/fb.png',
+                          ),
+                          onPressed: () {},
+                          iconSize: 37,
+                        ),
+                        IconButton(
+                          icon: Image.asset(
+                            'assets/images/insta.png',
+                          ),
+                          onPressed: () {},
+                          iconSize: 43,
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-              )
+              ),
             ],
           ),
         ),
--- lib/screens/ContactScreen.dart
+++ lib/screens/ContactScreen.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_portfolio/widgets/footer.dart';
