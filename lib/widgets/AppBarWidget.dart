2a721efd3392208487c85d7afdcfaf3814d51f37
Monik09 <monikinderjitsingh5@gmail.com>
2020-12-09 15:10:06 +0530

    Add title Signature

--- lib/widgets/AppBarWidget.dart
+++ lib/widgets/AppBarWidget.dart
@@ -5,61 +5,83 @@ Widget appBarWidget(BuildContext context) {
   double fontsize = 25;
   Color textColor = Color(0xff173266);
   return AppBar(
+    automaticallyImplyLeading: false,
     toolbarHeight: 120,
     elevation: 0,
     backgroundColor: Colors.transparent,
+    title: FlatButton(
+      padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.02),
+      child: Text(
+        "Monikinderjit Singh",
+        style: TextStyle(
+            fontFamily: 'TheRichJuliet',
+            fontSize: 55,
+            color:textColor,
+            fontWeight: FontWeight.w800),
+      ),
+      onPressed: null,
+    ),
     actions: [
       FlatButton(
         child: Text(
           "About Me",
           style: GoogleFonts.domine(
-            fontWeight: FontWeight.bold,
-            fontSize:fontsize,
+            fontWeight: FontWeight.w600,
+            fontSize: fontsize,
             color: textColor,
+            
           ),
         ),
         onPressed: null,
       ),
-      SizedBox(width: MediaQuery.of(context).size.width*0.02,),
+      SizedBox(
+        width: MediaQuery.of(context).size.width * 0.02,
+      ),
       // Spacer(),
       FlatButton(
         child: Text(
           "Skills",
           style: GoogleFonts.domine(
-            fontWeight: FontWeight.bold,
+            fontWeight: FontWeight.w600,
             fontSize: fontsize,
             color: textColor,
           ),
         ),
         onPressed: null,
       ),
-      SizedBox(width: MediaQuery.of(context).size.width*0.02,),
+      SizedBox(
+        width: MediaQuery.of(context).size.width * 0.02,
+      ),
       // Spacer(),
       FlatButton(
         child: Text(
           "Projects",
           style: GoogleFonts.domine(
-            fontWeight: FontWeight.bold,
+            fontWeight: FontWeight.w600,
             fontSize: fontsize,
             color: textColor,
           ),
         ),
         onPressed: null,
       ),
-      SizedBox(width: MediaQuery.of(context).size.width*0.02,),
+      SizedBox(
+        width: MediaQuery.of(context).size.width * 0.02,
+      ),
       // Spacer(),
       FlatButton(
         child: Text(
           "Contact Me",
           style: GoogleFonts.domine(
-            fontWeight: FontWeight.bold,
+            fontWeight: FontWeight.w600,
             fontSize: fontsize,
             color: textColor,
           ),
         ),
         onPressed: null,
       ),
-      SizedBox(width: MediaQuery.of(context).size.width*0.003,),
+      SizedBox(
+        width: MediaQuery.of(context).size.width * 0.003,
+      ),
       // Spacer(),
     ],
   );
