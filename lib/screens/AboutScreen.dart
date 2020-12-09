dd5edd9374132c43ce1124afecc57ff8d848f2c3
Monik09 <monikinderjitsingh5@gmail.com>
2020-12-09 14:59:04 +0530

    Small changes in About and appbar

--- lib/screens/AboutScreen.dart
+++ lib/screens/AboutScreen.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import '../widgets/AppBarWidget.dart';
 
 class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(189,224,255,1),
-      appBar: appBarWidget(),
+      backgroundColor: Color.fromRGBO(189, 224, 255, 1),
+      appBar: appBarWidget(context),
+      body: SingleChildScrollView(
+              child: Container(
+          padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height * 0.08),
+          // color: Colors.white,
+          height: MediaQuery.of(context).size.height * 0.9,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width/2,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 70.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Monikinderjit Singh",
+                      //   style: GoogleFonts.lora(
+                      //       color: Color(0xff173266), fontSize: 60,fontWeight: FontWeight.w800),
+                      // ),
+                      style:TextStyle(fontFamily: 'TheRichJuliet',fontSize: 60)),
+                      SizedBox(height: MediaQuery.of(context).size.height*0.1,),
+                      Text(
+                        "I love exploring new things and giving my best to make use of it",
+                        softWrap: true,
+                        style: GoogleFonts.lora(
+                            color: Colors.blueAccent[700], fontSize: 50),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right:50.0),
+                child: Container(
+                  alignment: Alignment.center,
+                  child: CircleAvatar(
+                    radius: MediaQuery.of(context).size.width * 0.12,
+                    backgroundColor: Colors.black,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/widgets/AppBarWidget.dart
+++ lib/widgets/AppBarWidget.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Widget appBarWidget() {
+Widget appBarWidget(BuildContext context) {
+  double fontsize = 25;
+  Color textColor = Color(0xff173266);
   return AppBar(
+    toolbarHeight: 120,
     elevation: 0,
     backgroundColor: Colors.transparent,
     actions: [
@@ -11,42 +14,52 @@ Widget appBarWidget() {
           "About Me",
           style: GoogleFonts.domine(
             fontWeight: FontWeight.bold,
-            fontSize: 19,
-            color: Color(0xff173266),
+            fontSize:fontsize,
+            color: textColor,
           ),
         ),
         onPressed: null,
       ),
+      SizedBox(width: MediaQuery.of(context).size.width*0.02,),
       // Spacer(),
       FlatButton(
         child: Text(
           "Skills",
           style: GoogleFonts.domine(
-            fontWeight: FontWeight.bold,fontSize: 19,color:Color(0xff173266),
+            fontWeight: FontWeight.bold,
+            fontSize: fontsize,
+            color: textColor,
           ),
         ),
         onPressed: null,
       ),
+      SizedBox(width: MediaQuery.of(context).size.width*0.02,),
       // Spacer(),
       FlatButton(
         child: Text(
           "Projects",
           style: GoogleFonts.domine(
-            fontWeight: FontWeight.bold,fontSize: 19,color:Color(0xff173266),
+            fontWeight: FontWeight.bold,
+            fontSize: fontsize,
+            color: textColor,
           ),
         ),
         onPressed: null,
       ),
+      SizedBox(width: MediaQuery.of(context).size.width*0.02,),
       // Spacer(),
       FlatButton(
         child: Text(
           "Contact Me",
           style: GoogleFonts.domine(
-            fontWeight: FontWeight.bold,fontSize: 19,color:Color(0xff173266),
+            fontWeight: FontWeight.bold,
+            fontSize: fontsize,
+            color: textColor,
           ),
         ),
         onPressed: null,
       ),
+      SizedBox(width: MediaQuery.of(context).size.width*0.003,),
       // Spacer(),
     ],
   );
