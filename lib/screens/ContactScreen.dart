71c7c61ccf4d9e5171b1c0b159ef9851d3f26292
Monik09 <monikinderjitsingh5@gmail.com>
2020-12-10 22:46:15 +0530

    Scrollbar And TextFieldS

--- lib/screens/ContactScreen.dart
+++ lib/screens/ContactScreen.dart
@@ -4,8 +4,17 @@ import 'package:my_portfolio/widgets/footer.dart';
 
 import '../widgets/appBarWidget.dart';
 
-class ContactScreen extends StatelessWidget {
+class ContactScreen extends StatefulWidget {
   static const String routeName = "/ContactScreen";
+
+  @override
+  _ContactScreenState createState() => _ContactScreenState();
+}
+
+class _ContactScreenState extends State<ContactScreen> {
+  TextEditingController _textEditingController1 = TextEditingController();
+  TextEditingController _textEditingController2 = TextEditingController();
+  TextEditingController _textEditingController3 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,19 +26,19 @@ class ContactScreen extends StatelessWidget {
           children: [
             Container(
               alignment: Alignment.center,
-              color: Color(0xff71bcff),
+              color:  Color(0xffe0ffbd),//Color.fromRGBO(224,255,189,1), //
               margin: EdgeInsets.symmetric(
                 horizontal: MediaQuery.of(context).size.width * 0.16,
                 vertical: MediaQuery.of(context).size.height * 0.1,
               ),
-              height: MediaQuery.of(context).size.height * 0.5,
+              height: MediaQuery.of(context).size.height * 0.58,
               width: MediaQuery.of(context).size.width * 0.7,
               child: Column(
                 // mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
 
                 mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(
                     'Contact with me if you wanna like to work together :)',
@@ -42,39 +51,94 @@ class ContactScreen extends StatelessWidget {
                     height: MediaQuery.of(context).size.height * 0.1,
                   ),
                   Container(
-                    color: Color(0xff71bcff),
-                    margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.01),
+                    color:
+                        Color.fromRGBO(224,255,189, 1), // Color(0xff71bcff),
+                    margin: EdgeInsets.all(
+                      MediaQuery.of(context).size.width * 0.006,
+                    ),
+                    width: MediaQuery.of(context).size.width * 0.5,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Container(
-                          width: MediaQuery.of(context).size.width * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.23,
+                          height: MediaQuery.of(context).size.height * 0.06,
                           child: ListTile(
                             title: Text('Name'),
-                            trailing: Text('Enter here'),
+                            trailing: Container(
+                              width: MediaQuery.of(context).size.width * 0.15,
+                              height: MediaQuery.of(context).size.height * 0.04,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  labelText: 'Name',
+                                  border: OutlineInputBorder(),
+                                ),
+                                controller: _textEditingController1,
+                                textCapitalization: TextCapitalization.words,
+                                keyboardType: TextInputType.name,
+                                onChanged: (name) {
+                                  print(name);
+                                },
+                                onSubmitted: (name) {},
+                              ),
+                            ), //Text('Enter here'),
                           ),
                         ),
                         Container(
-                          width: MediaQuery.of(context).size.width * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.23,
+                          height: MediaQuery.of(context).size.height * 0.06,
                           child: ListTile(
                             title: Text('Email'),
-                            trailing: Text('Enter here'),
+                            trailing: Container(
+                              width: MediaQuery.of(context).size.width * 0.15,
+                              height: MediaQuery.of(context).size.height * 0.04,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Email'),
+                                controller: _textEditingController2,
+                                keyboardType: TextInputType.emailAddress,
+                                onChanged: (email) {
+                                  print(email);
+                                },
+                                onSubmitted: (name) {},
+                              ),
+                            ),
                           ),
                         ),
                         Container(
-                          width: MediaQuery.of(context).size.width * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.23,
+                          height: MediaQuery.of(context).size.height * 0.12,
                           child: ListTile(
                             title: Text('Message'),
-                            trailing: Text('Enter here'),
+                            trailing: Container(
+                              width: MediaQuery.of(context).size.width * 0.15,
+                              height: MediaQuery.of(context).size.height * 0.07,
+                              child: TextField(
+                                expands: true,
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Message'),
+                                controller: _textEditingController3,
+                                // maxLines: 3,
+                                keyboardType: TextInputType.text,
+                                textCapitalization:
+                                    TextCapitalization.sentences,
+                                onChanged: (message) {
+                                  print(message);
+                                },
+                                onSubmitted: (name) {},
+                              ),
+                            ),
                           ),
                         ),
                       ],
                     ),
                   ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.07,
-                  ),
+                  // SizedBox(
+                  //   height: MediaQuery.of(context).size.height * 0.07,
+                  // ),
                   Center(
                     child: Container(
                       height: MediaQuery.of(context).size.height * 0.04,
@@ -92,16 +156,16 @@ class ContactScreen extends StatelessWidget {
                 ],
               ),
             ),
-            
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.bottomCenter,//FractionalOffset.bottomCenter,
-                      child: Padding(
-                          padding: EdgeInsets.only(bottom: 10.0),
-                          child: footer(context) //Your widget here,
-                          ),
+            Expanded(
+              child: Align(
+                alignment:
+                    Alignment.bottomCenter, //FractionalOffset.bottomCenter,
+                child: Padding(
+                    padding: EdgeInsets.only(bottom: 10.0),
+                    child: footer(context) //Your widget here,
                     ),
-                  ),
+              ),
+            ),
           ],
         ),
       ),
--- lib/screens/ProjectScreen.dart
+++ lib/screens/ProjectScreen.dart
@@ -1,124 +1,165 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_web_scrollbar/flutter_web_scrollbar.dart';
 
 import '../widgets/footer.dart';
 import '../widgets/appBarWidget.dart';
 
-class ProjectScreen extends StatelessWidget {
-  final ScrollController _scrollController = ScrollController();
+class ProjectScreen extends StatefulWidget {
   static const String routeName = '/ProjectScreen';
+
+  @override
+  _ProjectScreenState createState() => _ProjectScreenState();
+}
+
+class _ProjectScreenState extends State<ProjectScreen> {
+  ScrollController _scrollController = ScrollController();
+  @override
+  void initState() {
+    //Initialize the  scrollController
+    _scrollController = ScrollController();
+    super.initState();
+  }
+
+  void scrollCallBack(DragUpdateDetails dragUpdate) {
+    setState(() {
+      // Note: 3.5 represents the theoretical height of all my scrollable content. This number will vary for you.
+      _scrollController.position.moveTo(dragUpdate.globalPosition.dy *3.5);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarWidget(context),
       backgroundColor: Color.fromRGBO(189, 224, 255, 1),
-      body: Scrollbar(
-        isAlwaysShown: true,
-        // thickness: 20,
-        controller: _scrollController,
-        child: SafeArea(
-          child: SingleChildScrollView(
-            controller: _scrollController,
+      body: Stack(
+        children: [
+          Scrollbar(
+            // controller: _scrollController,
+            // isAlwaysShown: true,
             child: Container(
-              height: MediaQuery.of(context).size.height * 1.4,
-              child: Column(
-                children: [
-                  Row(
+              child: SingleChildScrollView(
+                controller: _scrollController,
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 1.4,
+                  child: Column(
                     children: [
-                      Container(
-                        padding: EdgeInsets.only(
-                          left: MediaQuery.of(context).size.width * 0.01,
-                        ),
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        height: MediaQuery.of(context).size.height * 0.2,
-                        child: Text(
-                          "Projects I created while learning and exploring different fields and in different courses..",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.tinos(
-                            fontSize: 50,
-                            color: Colors.blue[800],
-                            fontWeight: FontWeight.w700,
+                      Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(
+                              left: MediaQuery.of(context).size.width * 0.01,
+                            ),
+                            alignment: Alignment.center,
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            height: MediaQuery.of(context).size.height * 0.2,
+                            child: Text(
+                              "Projects I created while learning and exploring different fields and in different courses..",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.tinos(
+                                fontSize: 50,
+                                color: Colors.blue[800],
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
                           ),
-                        ),
+                          Container(
+                            child: Image.asset(
+                              'assets/images/work_re.png',
+                              scale: 0.74,
+                            ),
+                          ),
+                        ],
                       ),
                       Container(
-                        child: Image.asset(
-                          'assets/images/work_re.png',
-                          scale: 0.74,
+                        margin: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.03,
+                        ),
+                        color: Colors.white,
+                        height: MediaQuery.of(context).size.height * 0.6750,
+                        child: GridView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            childAspectRatio: 1.88,
+                          ),
+                          itemBuilder: (ctx, index) {
+                            if (index < 5)
+                              return Container(
+                                height:
+                                    MediaQuery.of(context).size.width * 0.17,
+                                width: MediaQuery.of(context).size.width * 0.32,
+                                color: Colors.transparent,
+                                child: Card(
+                                  color: Colors.blue[800],
+                                  child: Column(
+                                    children: [
+                                      Text('ABC'),
+                                      Text('date'),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            else if (index == 5) {
+                              return Container(
+                                height:
+                                    MediaQuery.of(context).size.width * 0.17,
+                                width: MediaQuery.of(context).size.width * 0.32,
+                                color: Colors.transparent,
+                                child: Card(
+                                  color: Colors.blue[800],
+                                  child: RaisedButton(
+                                    child: Text(
+                                      "More Projects",
+                                      style: GoogleFonts.oswald(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 50,
+                                        color: Colors.blue,
+                                      ),
+                                    ),
+                                    onPressed: null,
+                                  ),
+                                ),
+                              );
+                            } else
+                              return null;
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment
+                              .bottomCenter, //FractionalOffset.bottomCenter,
+                          child: Padding(
+                              padding: EdgeInsets.only(bottom: 10.0),
+                              child: footer(context) //Your widget here,
+                              ),
                         ),
                       ),
                     ],
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width * 0.03,
-                    ),
-                    color: Colors.white,
-                    height: MediaQuery.of(context).size.height * 0.6750,
-                    child: GridView.builder(
-                      controller: _scrollController,
-                      physics: NeverScrollableScrollPhysics(),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        childAspectRatio: 1.88,
-                      ),
-                      itemBuilder: (ctx, index) {
-                        if (index < 5)
-                          return Container(
-                            height: MediaQuery.of(context).size.width * 0.17,
-                            width: MediaQuery.of(context).size.width * 0.32,
-                            color: Colors.transparent,
-                            child: Card(
-                              color: Colors.blue[800],
-                              child: Column(
-                                children: [
-                                  Text('ABC'),
-                                  Text('date'),
-                                ],
-                              ),
-                            ),
-                          );
-                        else if (index == 5) {
-                          return Container(
-                            height: MediaQuery.of(context).size.width * 0.17,
-                            width: MediaQuery.of(context).size.width * 0.32,
-                            color: Colors.transparent,
-                            child: Card(
-                              color: Colors.blue[800],
-                              child: RaisedButton(
-                                child: Text(
-                                  "More Projects",
-                                  style: GoogleFonts.oswald(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 50,
-                                    color: Colors.blue,
-                                  ),
-                                ),
-                                onPressed: null,
-                              ),
-                            ),
-                          );
-                        } else
-                          return null;
-                      },
-                    ),
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment
-                          .bottomCenter, //FractionalOffset.bottomCenter,
-                      child: Padding(
-                          padding: EdgeInsets.only(bottom: 10.0),
-                          child: footer(context) //Your widget here,
-                          ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
+          Container(
+            height: MediaQuery.of(context).size.height*1.4,
+            child: FlutterWebScroller(
+              //Pass a reference to the ScrollCallBack function into the scrollbar
+              scrollCallBack,
+
+              //Add optional values
+              scrollBarBackgroundColor: Colors.grey[300],
+              scrollBarWidth: 20.0,
+              dragHandleColor: Colors.red,
+              dragHandleBorderRadius: 2.0,
+              dragHandleHeight: 500.0,
+              dragHandleWidth: 15.0,
+            ),
+          ),
+        ],
       ),
     );
   }
