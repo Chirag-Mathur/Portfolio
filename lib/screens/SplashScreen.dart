29cd42c5804ef2e1c7879d4fb197afb6b0712c61
Monik09 <monikinderjitsingh5@gmail.com>
2020-12-15 22:22:03 +0530

    ProgressIndicator-splash Screen

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 import './AboutScreen.dart';
 
@@ -37,9 +39,23 @@ class _SplashScreenState extends State<SplashScreen>
     setState(() {
       _visible = !_visible;
     });
+    progresser();
     // Timer(Duration(seconds: 6), () {
     //   Navigator.of(context).pushReplacementNamed(AboutScreen.routeName);
     // });
+  }
+
+  double value = 0.0;
+  void progresser() {
+    Timer.periodic(Duration(milliseconds: 10), (t) {
+      if (value < 1)
+        setState(() {
+          value += 0.0015;
+          print(value);
+        });
+      else 
+        t.cancel();
+    });
   }
 
   @override
@@ -52,26 +68,37 @@ class _SplashScreenState extends State<SplashScreen>
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(
+          children: [
+            Text(
               'M',
-              style:GoogleFonts.blackOpsOne(
-                 fontSize: 120,
-                  color: Colors.white,
-                  // fontWeight: FontWeight.w800
-              )
-              //  TextStyle(
-              //     fontFamily: 'BlackOpsOne',
-              //     fontSize: 120,
-              //     color: Colors.white,
-              //     fontWeight: FontWeight.w800),
+              style: GoogleFonts.blackOpsOne(
+                fontSize: 120,
+                color: Colors.white,
+                // fontWeight: FontWeight.w800
+              ),
             ),
+            
             Text(
               'Monikinderjit Singh',
               style: TextStyle(
                   fontFamily: 'TheRichJuliet',
                   fontSize: 70,
                   color: Colors.white,
-                  fontWeight: FontWeight.w600),
+                  fontWeight: FontWeight.w400),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height*0.15,),
+            Container(
+              width: 300,
+              height: 30,
+              child: LiquidLinearProgressIndicator(
+                value:(value<1)? value:1.7, // Defaults to 0.5.
+                valueColor: AlwaysStoppedAnimation(Colors
+                    .pink), // Defaults to the current Theme's accentColor.
+                backgroundColor: Colors.white,
+                borderWidth: 2.0,
+                borderColor: Colors.white,
+                borderRadius: 12.0,
+              ),
             ),
           ],
         ),
